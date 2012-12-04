702bd324d8c153b3bc337c95b1eb44c542abb828
Joshua Bates <joshuabates@gmail.com>
2012-12-03 16:44:12 -0800

    Port to javascript

---
+++ spec/jasmine_let_spec.js
@@ -0,0 +1,42 @@
+describe('jasmine.letHelper', function() {
+  this.let('subject', 'science')
+
+  it('should get subject', function() {
+    expect(this.get('subject')).toEqual('science')
+  })
+
+  describe('Nested', function() {
+    it('should get subject from parent scope', function() {
+      expect(this.get('subject')).toEqual('science')
+    })
+  })
+
+  describe('Nested override', function() {
+    this.let('subject', 'math')
+
+    it('should override subject from parent scope', function() {
+      expect(this.get('subject')).toEqual('math')
+    })
+  })
+
+  describe('Object value', function() {
+    this.let('subject', { difficulty: 'easy', name: 'rider' })
+
+    beforeEach(function() {
+      var subject = this.get('subject')
+      subject.name = 'nocando'
+    })
+    
+    it('should make object immutable', function() {
+      expect(this.get('subject').name).toEqual('rider')
+    })
+  })
+
+  describe('Function value', function() {
+    this.let('level', 2)
+    this.let('subject', function() { return "Math 2" })
+    it('should call subject method', function () {
+      expect(this.get('subject')).toEqual('Math 2')
+    }) 
+  })
+})
---
+++ src/jasmine-let.js
@@ -0,0 +1,29 @@
+jasmine.letHelper = (typeof jasmine.letHelper === 'undefined' ? {} : jasmine.letHelper)
+
+jasmine.letHelper.let = function(name, value) {
+  if(this.sandbox == undefined) this.sandbox = {}
+  
+  if(typeof value === 'object') {
+    this.sandbox[name] = function() { return jQuery.extend(true, {}, value) }
+  } else {
+    this.sandbox[name] = function() { return value }
+  }
+}
+
+jasmine.letHelper.get = function(name) {
+  var value
+  if(!this.sandbox || this.sandbox[name] == undefined) {
+    if(this.parentSuite) return this.parentSuite.get(name)
+  } else {
+    value = this.sandbox[name]()
+    if(typeof value === 'function') {
+      return value()
+    } else {
+      return value
+    }
+  }
+}
+
+jasmine.Suite.prototype.let  = jasmine.letHelper.let
+jasmine.Suite.prototype.get = jasmine.letHelper.get
+jasmine.Spec.prototype.get = function (name) { return this.suite.get(name) }
